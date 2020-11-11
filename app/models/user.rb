06c54da5f7f60ba2177967e8bc3df2a823f11600
kanalemon <kanalemon@icloud.com>
2020-11-11 14:45:06 +0900

    ログイン、ログアウト

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,10 +4,18 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-         validates :nikcname, presence: true
+         validates :nickname, presence: true
+         validates :birthday, presence: true
+
+      with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/} do 
          validates :family_name, presence: true
          validates :last_name, presence: true
+      end
+      with_options presence: true, format: { with: /\A[ァ-ン]+\z/} do
          validates :family_name_kana, presence: true
          validates :last_name_kana, presence: true
-         validates :birthday, presence: true
+      end
+
+      PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+      validates_format_of :password, with: PASSWORD_REGEX
 end
--- db/schema.rb
+++ db/schema.rb
@@ -13,12 +13,12 @@
 ActiveRecord::Schema.define(version: 2020_11_09_072426) do
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.string "nickname"
-    t.string "family_name"
-    t.string "last_name"
-    t.string "family_name_kana"
-    t.string "last_name_kana"
-    t.date "birthday"
+    t.string "nickname", null: false
+    t.string "family_name", null: false
+    t.string "last_name", null: false
+    t.string "family_name_kana", null: false
+    t.string "last_name_kana", null: false
+    t.date "birthday", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
     t.string "reset_password_token"

2ad04a3084588e6e973d537a1d16149f4f0c9335
kanalemon <kanalemon@icloud.com>
2020-11-13 17:11:03 +0900

    ActiveStorage導入

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,9 +1,10 @@
 class Item < ApplicationRecord
   belongs_to :users
-  has_one : purchaser
+  has_one :purchaser
   belongs_to :category
   belongs_to :status
   belongs_to :postage
   belongs_to :prefecture
   belongs_to :days
+  has_one_attached :image
 end

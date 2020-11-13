35fc93ecec45bbfff9cc91a5016a4b9c4488dbd9
kanalemon <kanalemon@icloud.com>
2020-11-13 16:52:04 +0900

    モデル、テーブル作成

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,9 @@
 class Item < ApplicationRecord
+  belongs_to :users
+  has_one : purchaser
+  belongs_to :category
+  belongs_to :status
+  belongs_to :postage
+  belongs_to :prefecture
+  belongs_to :days
 end

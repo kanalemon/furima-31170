fc35aa873466fa9a1919fd4b8b7591033dac90ff
kanalemon <kanalemon@icloud.com>
2020-11-18 18:32:32 +0900

    修正3

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,8 +36,10 @@ class ItemsController < ApplicationController
 
   def destroy
     if current_user.id == @item.user_id
-        @item.destroy
-        redirect_to root_path
+       @item.destroy
+      redirect_to root_path
+    else
+      render :show
     end
   end
 

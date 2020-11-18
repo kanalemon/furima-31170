f970e4a1fbc35c9aa3915b19291fc15faf384b25
kanalemon <kanalemon@icloud.com>
2020-11-18 12:25:36 +0900

    エラーメッセージの出力

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -29,9 +29,9 @@ class ItemsController < ApplicationController
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.update(item_params)
+    @item = Item.find(params[:id])
+    @item.update(item_params)
+    if @item.update(item_params)
       redirect_to item_path
     else
       render :edit

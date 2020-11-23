a21122ce4274ebebe7cee25327f5b08aabbae68f
kanalemon <kanalemon@icloud.com>
2020-11-23 17:57:47 +0900

    SoldOut表示

--- app/controllers/purchasers_controller.rb
+++ app/controllers/purchasers_controller.rb
@@ -3,7 +3,9 @@ class PurchasersController < ApplicationController
 
   def index
     @item = Item.find(params[:item_id])
-    if current_user.id != @item.user_id
+    if @item.purchaser != nil
+      redirect_to root_path
+    elsif current_user.id != @item.user_id
       @order = Order.new
     else
       redirect_to root_path
@@ -23,13 +25,12 @@ class PurchasersController < ApplicationController
   end
 
   private
-
   def purchaser_params
     params.require(:order).permit(:post_cord, :prefecture_id, :city, :address, :building_name, :phone_number).merge(item_id: params[:item_id], user_id: current_user.id, token: params[:token])
   end
 
   def pay_item
-    Payjp.api_key = ENV['PAYJP_SECRET_KEY']
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
     Payjp::Charge.create(
       amount: @item.price,
       card: purchaser_params[:token],
